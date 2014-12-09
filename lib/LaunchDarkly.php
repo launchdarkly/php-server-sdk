03360a6ce894d3caf9fd451a17cccc4f44241cea
Arun Bhalla <arun@shme.net>
2014-12-09 03:52:58 -0800

    Add FeatureRep and unit tests

---
+++ lib/LaunchDarkly.php
@@ -0,0 +1,6 @@
+<?php
+
+require_once(dirname(__FILE__) . "/LaunchDarkly/FeatureRep.php");
+require_once(dirname(__FILE__) . "/LaunchDarkly/LDUser.php");
+require_once(dirname(__FILE__) . "/LaunchDarkly/TargetRule.php");
+require_once(dirname(__FILE__) . "/LaunchDarkly/Variation.php");
