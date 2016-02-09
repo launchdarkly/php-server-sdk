6e2ee66100c134f273c110521b6bbedde6807f87
John Kodumal <john@launchdarkly.com>
2016-02-08 17:25:43 -0800

    Bump version to 0.7.0

--- src/LaunchDarkly/LDClient.php
+++ src/LaunchDarkly/LDClient.php
@@ -8,7 +8,7 @@ use Exception;
  */
 class LDClient {
     const DEFAULT_BASE_URI = 'https://app.launchdarkly.com';
-    const VERSION = '0.6.3';
+    const VERSION = '0.7.0';
 
     protected $_apiKey;
     protected $_baseUri;
