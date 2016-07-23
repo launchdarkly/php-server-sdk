4ad38ed01585f9a5ff164332a6f1d1785ee101d7
Dan Richelson <dan@launchdarkly.com>
2016-07-23 10:43:31 -0700

    FeatureRequester works with Guzzle 6, but no caching yet

--- src/LaunchDarkly/GuzzleFeatureRequester.php
+++ src/LaunchDarkly/GuzzleFeatureRequester.php
@@ -1,32 +1,36 @@
 <?php
 namespace LaunchDarkly;
 
-use GuzzleHttp\Client;
+use \GuzzleHttp\Client;
 use \GuzzleHttp\Exception\BadResponseException;
-use GuzzleHttp\HandlerStack;
+use \GuzzleHttp\HandlerStack;
 use Kevinrob\GuzzleCache\CacheMiddleware;
 
-class GuzzleFeatureRequester implements FeatureRequester {
+class GuzzleFeatureRequester implements FeatureRequester
+{
 
     private $_client;
+    private $_baseUri;
+    private $_defaults;
 
-    function __construct($baseUri, $apiKey, $options) {
-        $stack = HandlerStack::create();
-        $stack->push(new CacheMiddleware(), 'cache');
-        $this->_client = new Client(array(
-                                        'base_url' => $baseUri,
-                                        'handler' => $stack,
-                                        'defaults' => array(
-                                            'headers' => array(
-                                                'Authorization' => "api_key {$apiKey}",
-                                                'Content-Type' => 'application/json',
-                                                'User-Agent' => 'PHPClient/' . LDClient::VERSION
-                                            ),
-                                            'debug' => false,
-                                            'timeout' => $options['timeout'],
-                                            'connect_timeout' => $options['connect_timeout']
-                                        )
-                                    ));
+    function __construct($baseUri, $apiKey, $options)
+    {
+        $this->_baseUri = $baseUri;
+        error_log("uri: $baseUri");
+//        $stack = HandlerStack::create();
+//        $stack->push(new CacheMiddleware(), 'cache');
+
+        $this->_defaults = array(
+            'headers' => array(
+                'Authorization' => "api_key {$apiKey}",
+                'Content-Type' => 'application/json',
+                'User-Agent' => 'PHPClient/' . LDClient::VERSION
+            ),
+            'timeout' => $options['timeout'],
+            'connect_timeout' => $options['connect_timeout']
+        );
+
+        $this->_client = new Client();
     }
 
 
@@ -36,10 +40,15 @@ class GuzzleFeatureRequester implements FeatureRequester {
      * @param $key string feature key
      * @return array|null The decoded JSON feature data, or null if missing
      */
-    public function get($key) {
+    public function get($key)
+    {
         try {
-            $response = $this->_client->get("/api/eval/features/$key");
-            return $response->json();
+            $uri = $this->_baseUri . "/api/eval/features/$key";
+            error_log($uri);
+            $response = $this->_client->get($uri, $this->_defaults);
+            $body = $response->getBody();
+            error_log($body);
+            return json_decode($body, true);
         } catch (BadResponseException $e) {
             $code = $e->getResponse()->getStatusCode();
             error_log("GuzzleFeatureRetriever::get received an unexpected HTTP status code $code");
