2f7d54645798ee271dbccad5be7e5f8903560a28
Tim Younger <tim@webpt.com>
2016-12-17 23:05:41 -0700

    add travis config to test PHP versions 5.5 to 7.1. each PHP version is tested with the minimum and maximum dependency versions which provides some certainty that our dependency ranges in the composer config are valid. update feature-generator in integration-test to return json-object with all required keys and appropriate variation-structures. run integration tests in travis.

--- integration-tests/LDDFeatureRequesterTest.php
+++ integration-tests/LDDFeatureRequesterTest.php
@@ -3,6 +3,8 @@ namespace LaunchDarkly\Tests;
 
 require_once 'vendor/autoload.php';
 
+use LaunchDarkly\ApcLDDFeatureRequester;
+use LaunchDarkly\ApcuLDDFeatureRequester;
 use LaunchDarkly\LDClient;
 use LaunchDarkly\LDUserBuilder;
 
@@ -28,8 +30,10 @@ class LDDFeatureRetrieverTest extends \PHPUnit_Framework_TestCase {
                                         "scheme" => "tcp",
                                         "host" => 'localhost',
                                         "port" => 6379));
-        $client = new LDClient("BOGUS_API_KEY", array('feature_requester_class' => '\\LaunchDarkly\\ApcLDDFeatureRequester',
-            'apc_expiration' => 1));
+        $client = new LDClient("BOGUS_API_KEY", [
+            'feature_requester_class' => extension_loaded('apcu') ? ApcuLDDFeatureRequester::class : ApcLDDFeatureRequester::class,
+            'apc_expiration' => 1,
+        ]);
         $builder = new LDUserBuilder(3);
         $user = $builder->build();
 
@@ -42,24 +46,63 @@ class LDDFeatureRetrieverTest extends \PHPUnit_Framework_TestCase {
         $redis->hset("launchdarkly:features", 'foo', $this->gen_feature("foo", "baz"));
         $this->assertEquals("bar", $client->variation('foo', $user, 'jim'));
 
-        apc_delete("launchdarkly:features.foo");
+        if (extension_loaded('apcu')) {
+            \apcu_delete("launchdarkly:features.foo");
+        } else {
+            \apc_delete("launchdarkly:features.foo");
+        }
         $this->assertEquals("baz", $client->variation('foo', $user, 'jim'));
     }
 
     private function gen_feature($key, $val) {
-        $data = <<<EOF
-           {"name": "Feature $key", "key": "$key", "kind": "flag", "salt": "Zm9v", "on": true,
-            "variations": [{"value": "$val", "weight": 100,
-                            "targets": [{"attribute": "key", "op": "in", "values": []}],
-                            "userTarget": {"attribute": "key", "op": "in", "values": []}},
-                           {"value": false, "weight": 0,
-                            "targets": [{"attribute": "key", "op": "in", "values": []}],
-                            "userTarget": {"attribute": "key", "op": "in", "values": []}}],
-            "commitDate": "2015-09-08T21:24:16.712Z",
-            "creationDate": "2015-09-08T21:06:16.527Z",
-            "version": 4}
-EOF;
-         return $data;
+        $data = [
+            'name' => 'Feature ' . $val,
+            'key' => $key,
+            'kind' => 'flag',
+            'salt' => 'Zm9v',
+            'on' => true,
+            'variations' => [
+                $val,
+                false,
+            ],
+            'commitDate' => '2015-09-08T21:24:16.712Z',
+            'creationDate' => '2015-09-08T21:06:16.527Z',
+            'version' => 4,
+            'prerequisites' => [],
+            'targets' => [
+                [
+                    'values' => [
+                        $val,
+                    ],
+                    'variation' => 0,
+                ],
+                [
+                    'values' => [
+                        false,
+                    ],
+                    'variation' => 1,
+                ],
+            ],
+            'rules' => [],
+            'fallthrough' => [
+                'rollout' => [
+                    'variations' => [
+                        [
+                            'variation' => 0,
+                            'weight' => 95000,
+                        ],
+                        [
+                            'variation' => 1,
+                            'weight' => 5000,
+                        ],
+                    ],
+                ],
+            ],
+            'offVariation' => null,
+            'deleted' => false,
+        ];
+
+        return \json_encode($data);
     }
 
 }
