ce03bb967799eecc9f8e9a9817d3ca7f27c18fa3
LaunchDarklyCI <dev@launchdarkly.com>
2021-04-14 17:50:51 +0000

    Releasing version 3.7.6

--- src/LaunchDarkly/LDClient.php
+++ src/LaunchDarkly/LDClient.php
@@ -21,7 +21,7 @@ class LDClient
      * The current SDK version.
      * @var string
      */
-    const VERSION = '3.7.5';
+    const VERSION = '3.7.6';
 
     /** @var string */
     protected $_sdkKey;
