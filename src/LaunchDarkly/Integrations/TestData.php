9db1f9affd854e358f099dea4a505cc5afffbe11
Joey Malinowski <joemalin95@gmail.com>
2022-01-21 23:00:34 +0000

    continued implementing test data and tests for test data

--- src/LaunchDarkly/Integrations/TestData.php
+++ src/LaunchDarkly/Integrations/TestData.php
@@ -2,13 +2,48 @@
 
 namespace LaunchDarkly\Integrations;
 
+define('TRUE_VARIATION_INDEX', 0);
+define('FALSE_VARIATION_INDEX', 1);
+
 class TestData {
 
-    public function flag(string $key): FlagBuilder
+    /** @var array */
+    protected $_flagBuilders;
+
+    public function __construct()
     {
+        $this->_flagBuilders = [];
+    }
 
-        return new FlagBuilder($key);
+   /** 
+    * Creates or copies a `FlagBuilder` for building a test flag configuration.
 
+    * If this flag key has already been defined in this `TestData` instance, then the builder
+    * starts with the same configuration that was last provided for this flag.
+    *
+    * Otherwise, it starts with a new default configuration in which the flag has `True` and
+    * `False` variations, is `True` for all users when targeting is turned on and
+    * `False` otherwise, and currently has targeting turned on. You can change any of those
+    * properties, and provide more complex behavior, using the `FlagBuilder` methods.
+    *
+    * Once you have set the desired configuration, pass the builder to `update`.
+    *
+    * @param string $key: the flag key
+    * @return: FlagBuilder the flag configuration builder object
+    */
+    public function flag(string $key)
+    {
+        try {
+            //self._lock.rlock()
+            if (in_array($key, $this->_flagBuilders) && $this->_flagBuilders[$key]) {
+                return $this->_flagBuilders[$key]->copy();
+            } else {
+                $flagBuilder = new FlagBuilder($key);
+                return $flagBuilder->booleanFlag();
+            }
+        } finally {
+            //self._lock.runlock()
+        }
     }
 
 }
@@ -22,9 +57,9 @@ class FlagBuilder {
     /** @var array */
     protected $_variations;
     /** @var int|null */
-    protected $_off_variation;
+    protected $_offVariation;
     /** @var int|null */
-    protected $_fallthrough_variation;
+    protected $_fallthroughVariation;
     /** @var array */
     protected $_targets;
     /** @var array */
@@ -36,11 +71,10 @@ class FlagBuilder {
         $this->_key = $key;
         $this->_on = true;
         $this->_variations = [];
-        $this->_off_variation = null;
-        $this->_fallthrough_variation = null;
+        $this->_offVariation = null;
+        $this->_fallthroughVariation = null;
         $this->_targets = [];
         $this->_rules = [];
-
     }
 
     /**
@@ -57,13 +91,12 @@ class FlagBuilder {
 
         $to->_on = $this->_on;
         $to->_variations = $this->_variations;
-        $to->_off_variation = $this->_off_variation;
-        $to->_fallthrough_variation = $this->_fallthrough_variation;
+        $to->_offVariation = $this->_offVariation;
+        $to->_fallthroughVariation = $this->_fallthroughVariation;
         $to->_targets = $this->_targets;
         $to->_rules = $this->_rules;
 
         return $to;
-
     }
 
     /**
@@ -86,23 +119,122 @@ class FlagBuilder {
 
     }
 
+
+    /** 
+     * Specifies the fallthrough variation. The fallthrough is the value
+     * that is returned if targeting is on and the user was not matched by a more specific
+     * target or rule.
+
+     * If the flag was previously configured with other variations and the variation
+     * specified is a boolean, this also changes it to a boolean flag.
+
+     * @param bool/int $variation: `True` or `False` or the desired fallthrough variation index:
+     *                  `0` for the first, `1` for the second, etc.
+     * @return: FlagBuilder the flag builder
+     */ 
+    public function fallthroughVariation($variation)
+    {
+        if (is_bool($variation)) {
+            $this->booleanFlag()->_fallthroughVariation = $this->_variationForBoolean($variation);
+            return $this;
+        } else {
+            $this->_fallthroughVariation = $variation;
+            return $this;
+        }
+    }
+
+
+    /*  
+     *  Specifies the fallthrough variation. This is the variation that is returned
+     *  whenever targeting is off.
+     *
+     *  If the flag was previously configured with other variations and the variation
+     *  specified is a boolean, this also changes it to a boolean flag.
+     *
+     *  @param bool/int $variation: `True` or `False` or the desired off variation index:
+     *                   `0` for the first, `1` for the second, etc.
+     *  @return: FlagBuilder the flag builder
+     */
+    public function offVariation($variation)
+    {
+        if (is_bool($variation)) {
+            $this->booleanFlag()->_offVariation = $this->_variationForBoolean($variation);
+            return $this;
+        } else {
+            $this->_offVariation = $variation;
+            return $this;
+        }
+    }
+
+    /**
+     * A shortcut for setting the flag to use the standard boolean configuration.
+     *
+     * This is the default for all new flags created with
+     *      `$ldclient->integrations->test_data->TestData->flag()`.
+     *
+     * The flag will have two variations, `True` and `False` (in that order);
+     * it will return `False` whenever targeting is off, and `True` when targeting is on
+     * if no other settings specify otherwise.
+     *
+     * @return FlagBuilder the flag builder
+     */    
+    public function booleanFlag()
+    {
+        if ($this->_isBooleanFlag()) {
+            return $this;
+        } else {
+            return ($this->variations(true, false)
+                ->fallthroughVariation(TRUE_VARIATION_INDEX)
+                ->offVariation(FALSE_VARIATION_INDEX));
+        }
+    }
+
     /**    
-     *  Changes the allowable variation values for the flag. 
-     *  
-     *  The value may be of any valid JSON type. For instance, a boolean flag
-     *  normally has True, False; a string-valued flag might have
-     *  'red', 'green'; etc.
+     * Determines if the current flag is a boolean flag.
      *
-     *  Example: A single variation
+     * @return boolean true if flag is a boolean flag, false otherwise
+     */
+    private function _isBooleanFlag()
+    {
+        return (count($this->_variations) == 2
+            && $this->_variations[TRUE_VARIATION_INDEX] == True
+            && $this->_variations[FALSE_VARIATION_INDEX] == False);
+    }
+    
+
+    /**    
+     * Converts a boolean to the corresponding variation index.
      *
-     *      $td->flag('new-flag')->variations(True)
+     * @param boolean $variation the boolean variation value
+     * @return int the variation index for the given boolean
+     */
+    private function _variationForBoolean($variation)
+    {
+        if ($variation) {
+            return TRUE_VARIATION_INDEX;
+        } else {
+            return FALSE_VARIATION_INDEX;
+        }
+    }
+
+
+    /**    
+     * Changes the allowable variation values for the flag. 
+     * 
+     * The value may be of any valid JSON type. For instance, a boolean flag
+     * normally has True, False; a string-valued flag might have
+     * 'red', 'green'; etc.
      *
-     *  Example: Multiple variations
+     * Example: A single variation
      *
-     *      $td->flag('new-flag')->variations('red', 'green', 'blue')
+     *     $td->flag('new-flag')->variations(True)
      *
-     *  @param array $variations the the desired variations
-     *  @return FlagBuilder the flag builder object
+     * Example: Multiple variations
+     *
+     *     $td->flag('new-flag')->variations('red', 'green', 'blue')
+     *
+     * @param array $variations the the desired variations
+     * @return FlagBuilder the flag builder object
      */    
     public function variations(...$variations): FlagBuilder
     {
@@ -116,15 +248,55 @@ class FlagBuilder {
         return $this;
     }
 
+    /**
+     * Removes any existing targets from the flag.
+     * This undoes the effect of methods like
+     *      `$ldclient->integrations->test_data->FlagBuilder->variationForUser()`
+     *
+     * @return FlagBuilder the same flag builder
+     */
+    public function clearTargets()
+    {
+        $this->_targets = []; 
+        return $this;
+    }
+
+    /**
+     * Creates a dictionary representation of the flag
+     *
+     * @param int $version: the version number of the rule
+     * @return: the array representation of the flag
+     */ 
     public function build(int $version): array
     {
-        $base_flag_object = [
+        $baseFlagObject = [
             'key'        => $this->_key,
             'version'    => $version,
             'on'         => $this->_on,
             'variations' => $this->_variations
         ];
-        return $base_flag_object;
+
+        $baseFlagObject['offVariation'] = $this->_offVariation;
+        $baseFlagObject['fallthrough'] = [
+            'variation' => $this->_fallthroughVariation
+        ];
+
+        $targets = [];
+        foreach ($this->_targets as $varIndex => $userKeys) {
+            array_push($targets, [
+                'variation' => $varIndex,
+                'values' => $userKeys
+            ]);
+        }
+        $baseFlagObject['targets'] = $targets;
+
+        $baseFlagObject['rules'] = [];
+
+        foreach ($this->_rules as $idx => $rule) {
+            array_push($baseFlagObject['rules'], $rule->build($idx));
+        }
+
+        return $baseFlagObject;
     }
 
 }
--- tests/Integrations/TestDataTest.php
+++ tests/Integrations/TestDataTest.php
@@ -7,6 +7,11 @@ use LaunchDarkly\Integrations\TestData;
 
 class TestDataTest extends TestCase
 {
+    public function initializesWithEmptyData()
+    {
+
+    }
+
     public function testMakesFlag()
     {
         $td = new TestData();
@@ -33,6 +38,111 @@ class TestDataTest extends TestCase
         $this->assertEquals([1,2,3], $flag->build(0)['variations']);
         $this->assertEquals([4,5,6], $flagCopy->build(0)['variations']);
 
+    }
+
+    public function testFlagBuilderCanSetFallthroughVariation()
+    {
+        $td = new TestData();
+        $flag = $td->flag('test-flag');
+        $flag->fallthroughVariation(2);
+
+        $this->assertEquals(['variation' => 2], $flag->build(0)['fallthrough']);
+    }
+
+    public function testFlagConfigSimpleBoolean()
+    {
+        $td = new TestData();
+
+        $flag = $td->flag('test-flag-1');
+        $flagBuild = $flag->build(0);
+        $this->assertEquals(true, $flagBuild['on']);
+        $this->assertEquals(['variation' => 0], $flagBuild['fallthrough']);
+
+        $flagBool = $td->flag('test-flag-2')->booleanFlag();
+        $flagBoolBuild = $flagBool->build(0);
+        $this->assertEquals(true, $flagBoolBuild['on']);
+        $this->assertEquals(['variation' => 0], $flagBoolBuild['fallthrough']);
+
+        $flagOn = $td->flag('test-flag-3')->on(true);
+        $flagOnBuild = $flagOn->build(0);
+        $this->assertEquals(true, $flagOnBuild['on']);
+        $this->assertEquals(['variation' => 0], $flagOnBuild['fallthrough']);
+
+        $flagOff = $td->flag('test-flag-4')->on(false);
+        $flagOffBuild = $flagOff->build(0);
+        $this->assertEquals(false, $flagOffBuild['on']);
+        $this->assertEquals(['variation' => 0], $flagOffBuild['fallthrough']);
+
+        $flagFallthroughAndOffVariation = $td->flag('test-flag-5')
+                                             ->fallthroughVariation(true)
+                                             ->offVariation(false);
+        $flagFallthroughAndOffVariationBuild = $flagFallthroughAndOffVariation->build(0);
+        $this->assertEquals(true, $flagFallthroughAndOffVariationBuild['on']);
+        $this->assertEquals(['variation' => 0], $flagFallthroughAndOffVariationBuild['fallthrough']);
+    }
+
+    public function testFlagBuilderClearTargets() 
+    {
+        $td = new TestData();
+        $flag = $td->flag('test-flag')
+                    // TODO: Fill the flag with targets
+                   ->clearTargets()
+                   ->build(0);
+        $this->assertEquals([], $flag['targets']);
+    }
+
+    public function testFlagBuilderBooleanConfigMethodsForcesFlagToBeBoolean()
+    {
+        $td = new TestData();
+        $overwriteBoolFlag1 = $td->flag('test-flag')->variations(1, 2)->booleanFlag()->build(0);
+        $this->assertEquals([true, false], $overwriteBoolFlag1['variations']);
+        $this->assertEquals(true, $overwriteBoolFlag1['on']);
+        $this->assertEquals(1, $overwriteBoolFlag1['offVariation']);
+        $this->assertEquals(['variation' => 0], $overwriteBoolFlag1['fallthrough']);
+
+        $overwriteBoolFlag2 = $td->flag('test-flag')->variations(true, 2)->booleanFlag()->build(0);
+        $this->assertEquals([true, false], $overwriteBoolFlag2['variations']);
+        $this->assertEquals(true, $overwriteBoolFlag2['on']);
+        $this->assertEquals(1, $overwriteBoolFlag2['offVariation']);
+        $this->assertEquals(['variation' => 0], $overwriteBoolFlag2['fallthrough']);
+
+        $boolFlag = $td->flag('test-flag')->booleanFlag()->build(0);
+        $this->assertEquals([true, false], $boolFlag['variations']);
+        $this->assertEquals(true, $boolFlag['on']);
+        $this->assertEquals(1, $boolFlag['offVariation']);
+        $this->assertEquals(['variation' => 0], $boolFlag['fallthrough']);
+    }
+
+    public function testFlagConfigStringVariations()
+    {
+        $td = new TestData();
+        $stringVariationFlag = $td->flag('test-flag')
+                                    ->variations('red', 'green', 'blue')
+                                    ->offVariation(0)
+                                    ->fallthroughVariation(2)
+                                    ->build(0);
+        $this->assertEquals(['red', 'green', 'blue'], $stringVariationFlag['variations']);
+        $this->assertEquals(true, $stringVariationFlag['on']);
+        $this->assertEquals(0, $stringVariationFlag['offVariation']);
+        $this->assertEquals(['variation' => 2], $stringVariationFlag['fallthrough']);
+    }
+
+    public function testFlagBuilderDefaultsToBooleanFlag()
+    {
+        $td = new TestData();
+        $flag = $td->flag('empty-flag');
+        $this->assertEquals([true, false], $flag->build(0)['variations']);
+        $this->assertEquals(['variation' => 0], $flag->build(0)['fallthrough']);
+        $this->assertEquals(1, $flag->build(0)['offVariation']);
+    }
+
+    public function testFlagbuilderCanTurnFlagOff()
+    {
+        $td = new TestData();
+        $flag = $td->flag('test-flag');
+        $flag->on(false);
+
+        $this->assertEquals(false, $flag->build(0)['on']);
     }
 
     public function testSetsVariations()
