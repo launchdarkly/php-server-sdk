dd795782c722d7e648272c1f4188e7c9d5d18ca7
LaunchDarklyReleaseBot <launchdarklyreleasebot@launchdarkly.com>
2022-02-16 18:36:12 +0000

    executing delegated release

---
+++ .ldrelease/circleci/mac/execute.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -ue
+
+# Performs a delegated release step in a CircleCI Mac container. This mechanism is described
+# in scripts/circleci/README.md. All of the necessary environment variables should already be
+# in the generated CircleCI configuration.
+
+STEP="$1"
+SCRIPT="$2"
+echo
+echo "[${STEP}] executing ${SCRIPT}"
+"./${SCRIPT}"
